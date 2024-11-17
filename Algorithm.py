30a768e3402d7f4edec3be30288d4800c00704fb
Maanav746 <manavsmurf@gmail.com>
2024-11-17 16:10:32 +0100

    Add files via upload

---
+++ Algorithm.py
@@ -0,0 +1,24 @@
+import random
+import string
+def check_user_password(user: str, password: str, dictionary: dict):
+    for user1 in dictionary.keys():
+        if user == user1:
+            new_user = user + str(random.randint(0, 9))
+            print(f"Username {user} is already taken. Suggested username: {new_user}.")
+    for password1 in dictionary.values():
+        if password == password1:
+            letters = ''.join(random.choices(string.ascii_letters, k=2))
+            number = str(random.randint(0, 9)) 
+            new_password = password + letters + number
+            print(f"Password {password} is already taken. Suggested password: {new_password}")
+
+
+user_db = {
+    "john_doe": "password123",
+    "jane_smith": "securepass",
+    "admin": "admin123",
+    "guest": "guest123",
+    "charlie_brown": "peanuts2023"
+}
+print(check_user_password("new_user", "securepass", user_db))
+print(check_user_password("john_doe", "newpass", user_db))
